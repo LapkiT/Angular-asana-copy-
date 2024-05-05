d5bb831b963969df0e3bfd8a276cef94b793a804
Тимур Гаджибеков <timurgadzibekov@MacBook-Air-Timur.local>
2024-05-05 17:53:09 +0300

    update component task-form

--- task-tracker/src/app/Shared/components/task-form/task-form.component.ts
+++ task-tracker/src/app/Shared/components/task-form/task-form.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TaskServiceService } from '../../Services/task-service.service';
+import { Priority, Status } from '../../interfaces/task-models';
 
 @Component({
   selector: 'app-task-form',
@@ -7,6 +11,43 @@ import { Component } from '@angular/core';
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss'
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
+  taskForm!: FormGroup;
+  taskId!: string | null;
 
+  constructor(private fb: FormBuilder, private taskService: TaskServiceService, private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.taskId = this.route.snapshot.paramMap.get('id');
+    
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      deadline: ['', Validators.required],
+      priority: [Priority.Medium, Validators.required],
+      status: [Status.ToDo, Validators.required],
+      assignees: ['', Validators.required]
+    });
+
+    if (this.taskId) {
+      const task = this.taskService.getTaskById(this.taskId);
+      if (task) {
+        this.taskForm.patchValue(task);
+      }
+    }
+  }
+
+  onSubmit(): void {
+    if (this.taskForm.valid) {
+      const taskData = this.taskForm.value;
+
+      if (this.taskId) {
+        this.taskService.updateTask({ id: this.taskId, ...taskData });
+      } else {
+        const newTaskId = Date.now().toString(); 
+        this.taskService.addTask({ id: newTaskId, ...taskData });
+      }
+
+      this.router.navigate(['/tasks']);
+    }
+  }
 }
